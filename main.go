52b3147d4779b5a8834495b47468432daaca0db2
blacklycoriss <prokopovaleksandr544@gmail.com>
2026-03-07 13:06:54 +0700

    Feat: add middleware

--- main.go
+++ main.go
@@ -24,8 +24,10 @@ func main() {
 	//Add context
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
+
 	//Add default handler
 	opts := []bot.Option{
+		bot.WithMiddlewares(showMessageWithUserID, showMessageWithUserName),
 		bot.WithDefaultHandler(handler),
 	}
 	log.Println("Create bot options")
@@ -35,6 +37,7 @@ func main() {
 	if err != nil {
 		log.Panicf("Can't create bot with error %s", err)
 	}
+
 	//Bot start
 	b.Start(ctx)
 	log.Println("Bot started")
@@ -46,4 +49,22 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		ChatID: update.Message.Chat.ID,
 		Text:   update.Message.Text,
 	})
+}
+
+func showMessageWithUserID(next bot.HandlerFunc) bot.HandlerFunc {
+	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
+		if update.Message != nil {
+			log.Printf("%d say: %s", update.Message.From.ID, update.Message.Text)
+		}
+		next(ctx, b, update)
+	}
+}
+
+func showMessageWithUserName(next bot.HandlerFunc) bot.HandlerFunc {
+	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
+		if update.Message != nil {
+			log.Printf("%s say: %s", update.Message.From.FirstName, update.Message.Text)
+		}
+		next(ctx, b, update)
+	}
 }
