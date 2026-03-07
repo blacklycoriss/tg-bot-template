bf8c61e3ff0eb1c9f5932b9b07e3ce15a0f467a8
blacklycoriss <prokopovaleksandr544@gmail.com>
2026-03-07 13:00:49 +0700

    Feat: added log file

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ import (
 )
 
 func main() {
+	//Create log file
+	file, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file:", err)
+	}
+	log.SetOutput(file)
+	defer file.Close()
+
 	log.Println("Main started")
 
 	//Add context
